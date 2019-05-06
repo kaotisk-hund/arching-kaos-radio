be8d3c158fcd18721b6412841ada068c0cd2c67a
Kaotisk Hund <ccc2007chaos@gmail.com>
2019-05-06 05:08:50 +0300

    Added a sliding menu
    It's not configured yet TODO

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import RadioPlayer from './RadioPlayer';
 import Signature from './Signature';
 import ShowList from './ShowList';
 import NowPlaying from './NowPlaying';
+import MenuContainer from './MenuContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header>
+        <MenuContainer/>
         <RadioPlayer audioSource="http://radio.arching-kaos.tk:8000/demo.ogg"/>
         <NowPlaying/>
         <ShowList/>
