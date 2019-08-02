8f6efe67323752de449ffe451cf4a496b9dd9abb
kaotisk <ccc2007chaos@gmail.com>
2019-08-02 04:18:17 +0300

    Minor text update :)

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Welcome to Arching Kaos Radio. Enjoy music!
+        Welcome to Arching Kaos Radio<br/>Enjoy music!
       </p>
       <a
         className="App-link"
