cd0e2b72aeb5f030bfc966197d522edb6eab7325
kaotisk <ccc2007chaos@gmail.com>
2019-08-13 04:05:47 +0300

    mfe requirement: RoadMap #### A button "Shows/Listen on demand" Committing this changes I get the shows on the site. For now, I load a json file. Soon this will be a ssb feed. :)

--- src/ShowList.js
+++ src/ShowList.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-//import './NewComponent.css';
+import './ShowList.css';
+import Player from './Player'
+import Lists from './lists.json';
 
 class ShowList extends Component {
+  constructor(props, context) {
+  	super(props, context);
+ 
+  	this.state = {
+  		mixes : Lists
+  	}
+  }
   render() {
-    return (
-      <div></div>
-    );
+  	return (    
+      <div>
+      <h3>Mix list</h3>
+        <div class="ShowList">
+          
+        	{this.state.mixes.map(function(obj, idx){
+        		return (<table><td key={idx}>{obj.artist} - {obj.title}</td>
+        		<td width="10%"><Player audioSource={obj.ipfs}/></td></table>)
+        	})}
+          
+        </div>
+      </div>
+    )
+
   }
 }
 
