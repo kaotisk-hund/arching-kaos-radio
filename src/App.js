f7bcc64abd68822d70b948bcb92a4b9b9b268109
kaotisk <kaotiskhund@gmail.com>
2019-09-24 18:26:58 +0300

    Started commenting the code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+/*
+ * Kaotisk Hund 2019
+ * Βασική εφαρμογή του ιστότοπού μας 
+ *
+ */
+
 import React, { Component } from 'react';
 import Header from './Header';
 import RadioPlayer from './RadioPlayer';
@@ -6,14 +12,17 @@ import ShowList from './ShowList';
 import NowPlaying from './NowPlaying';
 import MenuContainer from './MenuContainer';
 
+/* Class της εφαρμογής μας που προβάλει τον ιστότοπο του σταθμού μας */
+
 class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      radio: "http://radio.arching-kaos.tk:8000/demo.ogg"
+      radio: "http://radio.arching-kaos.tk:8000/demo.ogg" /* Θέτουμε την πηγή του σταθμού μας εδώ */
     }
   }
   render() {
+    /* Η βασική διάταξη της ιστοσελίδας μας */
     return (
       <div className="App">
         <Header>
