cc08d1416ad9fcd4ddb976b44b48a838291ba4db
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 08:17:00 +0200

    Added restart always flag

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t arching-kaos-radio .
-docker run --name arching-kaos-radio -d -p 5000:5000 arching-kaos-radio
+docker run --name arching-kaos-radio --restart always -d -p 5000:5000 arching-kaos-radio
