b979478dd03e95a4014bfbeb6e1432cd2913008f
kaotisk <kaotisk@arching-kaos.com>
2021-08-21 20:07:07 +0300

    Forgot to add to pod

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 podman build -t arching-kaos-radio .
-podman run --name arching-kaos-radio --restart always -d arching-kaos-radio
+podman run --pod arching-kaos --name arching-kaos-radio --restart always -d arching-kaos-radio
